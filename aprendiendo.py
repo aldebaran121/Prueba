545538965bde3c8ef2e85edc506d064ad5e093f9
Anthony <anthonycampos8421@gmail.com>
2024-02-18 12:20:30 -0500

    poniendo ambos

--- aprendiendo.py
+++ aprendiendo.py
@@ -1,1 +1,16 @@
-print("hola puto")
+
+while True:
+    monto = float(input("Ingrese el monto de su sueldo: "))
+    if monto > 500:
+        print("Usted puede seguir con el programa")
+        False
+    else: 
+        print("Su monto no es el adecuado, intentelo nuevamente")
+
+comprobar = monto 
+
+monto = float(input("Ingrese el nuevo monto: "))
+
+while comprobar == monto:
+    monto = float(input("El monto ingresado es el mismo, ingrese uno diferente: "))
+
